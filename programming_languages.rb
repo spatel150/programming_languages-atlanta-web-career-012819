a1b1d1590844b457c78497c61aee2cd11e8aae60
Samidh Patel <spatel13456@gmail.com>
2019-01-17 03:31:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,5 +2,6 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |method, language_type| 
     language_type.each do |name, type|
-      if new_hash.has
+      if new_hash.has_key?(name)
+        
 end
